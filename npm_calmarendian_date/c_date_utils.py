433f73b088fb94eee3c3a6b1c2fad7af2c737208
Natasha Moorfield <mailbox@natashamoorfield.co.uk>
2022-08-07 11:43:32 +0100

    Add initial thoughts on date-time-struct

--- npm_calmarendian_date/c_date_utils.py
+++ npm_calmarendian_date/c_date_utils.py
@@ -1,7 +1,8 @@
 """
-Common utility functions used in various parts of the library.
+Common utility functions and classes used in various parts of the library.
 """
 import math
+from dataclasses import dataclass
 from typing import Union, Tuple
 
 RealNumber = Union[int, float]
@@ -31,3 +32,18 @@ def round_half_away_from_zero(number: RealNumber) -> int:
         wp += 1
     # Restore the sign before returning...
     return wp * (-1 if number < 0 else +1)
+
+
+@dataclass
+class DateTimeStruct(object):
+    """
+    A structure to hold the minimum possible data required to uniquely identify a date, time of date-time value.
+
+    it is analogous to the standard Python time.time_struct named tuple (particularly in that is appears to serve
+    no real purpose) except that (i) only integer values are stored, fractional seconds are represented as
+    microseconds; (ii) there is no weekday value (in Calmarendian dates, the day-of-the-week is an integral part of
+    the date; (iii) day-of-the-cycle (equivalent to day-of-the-year) and standard time offset are omitted - they are
+    derived, not defining, data; (iv) apart from the fact that daylight saving time is utter BS in almost any
+    circumstances, on an (effectively) equinoctial planet, the concept is completely meaningless.
+    """
+    pass
