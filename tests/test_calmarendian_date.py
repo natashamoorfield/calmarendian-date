26c235b27bb62a2d7879cfc7835bc60bc442d4a7
Natasha Moorfield <mailbox@natashamoorfield.co.uk>
2022-08-15 18:44:06 +0100

    Add test data for day-in-season-notation

--- tests/test_calmarendian_date.py
+++ tests/test_calmarendian_date.py
@@ -17,6 +17,7 @@ class DataSetOneItem:
     adr: int
     base_elements: tuple
     csn: str
+    dsn: str
     gcn: str = field(init=False)
 
     def __post_init__(self):
@@ -27,57 +28,68 @@ DATA_SET_ONE = [
     DataSetOneItem(
         adr=CDateConfig.MIN_ADR,
         base_elements=(0, 1, 1, 1, 1, 0, 0, 0, 0, 0),
-        csn="699-1-01-1 BZ"
+        csn="699-1-01-1 BZ",
+        dsn="1 Midwinter 699 BZ"
     ),
     DataSetOneItem(
         adr=-30_825,
         base_elements=(0, 688, 4, 5, 6, 0, 0, 0, 0, 0),
-        csn="012-4-05-6 BZ"
+        csn="012-4-05-6 BZ",
+        dsn="34 Perihelion 12 BZ"
     ),
     DataSetOneItem(
         adr=0,
         base_elements=(0, 700, 7, 51, 8, 0, 0, 0, 0, 0),
-        csn="000-7-51-8 BZ"
+        csn="000-7-51-8 BZ",
+        dsn="358 Onset 0 BZ"
     ),
     DataSetOneItem(
         adr=1,
         base_elements=(1, 1, 1, 1, 1, 0, 0, 0, 0, 0),
-        csn="001-1-01-1"
+        csn="001-1-01-1",
+        dsn="1 Midwinter 1"
     ),
     DataSetOneItem(
         adr=1_035_926,
         base_elements=(1, 423, 1, 23, 4, 0, 0, 0, 0, 0),
-        csn="423-1-23-4"
+        csn="423-1-23-4",
+        dsn="158 Midwinter 423"
     ),
     DataSetOneItem(
         adr=1_718_111,
         base_elements=(2, 1, 1, 2, 3, 0, 0, 0, 0, 0),
-        csn="701-1-02-3"
+        csn="701-1-02-3",
+        dsn="10 Midwinter 701"
     ),
     DataSetOneItem(
         adr=1_905_361,
         base_elements=(2, 77, 3, 4, 5, 0, 0, 0, 0, 0),
-        csn="777-3-04-5"
-    ),
-    DataSetOneItem(
-        adr=1_906_784,
-        base_elements=(2, 77, 7, 7, 7, 0, 0, 0, 0, 0),
-        csn="777-7-07-7"
+        csn="777-3-04-5",
+        dsn="26 Spring 777"
     ),
     DataSetOneItem(
         adr=CDateConfig.APOCALYPSE_EPOCH_ADR,
         base_elements=(2, 77, 7, 2, 7, 0, 0, 0, 0, 0),
-        csn="777-7-02-7"
+        csn="777-7-02-7",
+        dsn="21 Onset 777"
+    ),
+    DataSetOneItem(
+        adr=1_906_784,
+        base_elements=(2, 77, 7, 7, 7, 0, 0, 0, 0, 0),
+        csn="777-7-07-7",
+        dsn="49 Onset 777"
     ),
     DataSetOneItem(
         adr=24_541_844,
         base_elements=(15, 199, 7, 51, 4, 0, 0, 0, 0, 0),
-        csn="9999-7-51-4"
+        csn="9999-7-51-4",
+        dsn="354 Onset 9999"
     ),
     DataSetOneItem(
         adr=CDateConfig.MAX_ADR,
         base_elements=(99, 700, 7, 51, 8, 0, 0, 0, 0, 0),
-        csn="69300-7-51-8"
+        csn="69300-7-51-8",
+        dsn="358 Onset 69300"
     )
 ]
 
@@ -352,6 +364,7 @@ class SecondaryConstructorsTests(unittest.TestCase):
         for index, item in enumerate(data):
             with self.subTest(i=index):
                 with self.assertRaises(CalmarendianDateError):
+                    # noinspection PyTypeChecker
                     CalmarendianDate.from_date_time_struct(item)
 
 
